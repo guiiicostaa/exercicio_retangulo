65f24caf202e2f4bce88b68d556604e39c84988c
guiiicostaa <anchieta.guilherme@outlook.com>
2024-04-19 16:00:27 -0300

    feat: exercicio de calculo do retangulo

---
+++ exRETANGULO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace exRETANGULO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double b, h, area, peri, diagonal;
+
+
+
+            Console.Write("Base do retangulo: ");
+            b = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Base a altura do retangulo: ");
+            h = double.Parse(Console.ReadLine(), CI);
+
+            
+            area = b * h;
+            peri = 2 * b + 2 * h;
+            diagonal = Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2));
+
+            Console.WriteLine();
+            Console.WriteLine("Aera= " + area.ToString("F4", CI));
+            Console.WriteLine("Perimetro= " + peri.ToString("F4", CI));
+            Console.WriteLine("Diagonal= " + diagonal.ToString("F4", CI));
+
+        }
+    }
+}
